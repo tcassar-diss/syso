e3e64f764dff5aed888b86b6ade1e3716a1e597f
Tom Cassar <cassar.thomas.e@gmail.com>
2025-01-03 13:23:01 +0000

    Simplify stackparser implementation
    - O(n) search through return pointers for first non-libc path
    - Slower than cacheing where libc is: can speed up later if need be
        - Could cache libc address and find correct rp in userspace
        - Could be faster still and report it directly from kernel space
    - Until it becomes an issue, better to stick with simpler userspace code

--- addrspace/stackparse.go
+++ addrspace/stackparse.go
@@ -1,7 +1,6 @@
 package addrspace
 
 import (
-	"errors"
 	"fmt"
 
 	"go.uber.org/zap"
@@ -9,85 +8,61 @@ import (
 
 const LibcPath = "/usr/lib/x86_64-linux-gnu/libc.so.6"
 
-var ErrNoLibc = errors.New("no libc in process address space")
-
-type libcRange struct {
-	start, end uint64
-}
-
-func (l *libcRange) contains(rp uint64) bool {
-	return rp >= l.start && rp < l.end
-}
-
 // StackParser will use stack traces to identify the library responsible
 // for calling libc syscall wrapper.
 type StackParser struct {
-	logger     *zap.SugaredLogger
-	maps       *ProcMaps
-	libcRanges map[int32]*libcRange
+	logger *zap.SugaredLogger
+	maps   *ProcMaps
+	seen   bool
 }
 
 func NewStackParser(logger *zap.SugaredLogger, maps *ProcMaps) *StackParser {
 	return &StackParser{
-		logger:     logger,
-		maps:       maps,
-		libcRanges: make(map[int32]*libcRange),
+		logger: logger,
+		maps:   maps,
 	}
 }
 
 // AssignPC assigns a trace of return pointers to a shared library by PID.
-func (s *StackParser) AssignPC(pid int32, trace []uint64, dirty bool) (string, error) {
-	rge, err := s.loadLibcRange(pid, dirty)
-	if err != nil {
-		// todo: would it be a suitable fallback to assign top of
-		// stack with procmaps in this case? maybe better to handle one level higher.
-		return "", fmt.Errorf("failed to load libc address range: %w", err)
+func (s *StackParser) AssignPC(pid int32, trace [64]uint64, dirty bool) (string, error) {
+	if trace[len(trace)-1] != 0 {
+		s.logger.Warn("deepest frame non-0 => stacktrace may not be deep enough", "pid", pid)
 	}
 
-	callsiteRp := uint64(0)
+	var (
+		lib string
+		err error
+	)
 
 	for _, rp := range trace {
-		if rge.contains(rp) {
+		if rp == 0 {
+			break
+		}
+
+		lib, err = s.maps.AssignPC(rp, pid, dirty)
+		if err != nil {
+			s.logger.Warnw("failed to assign return pointer to memory map",
+				"pid", pid,
+				"rp", rp,
+				"err", err,
+			)
+
+			// try to keep going up the stack - see if we end up somewhere that we expect
+			// todo: decide if this is the correct intended behaviour
+
 			continue
 		}
 
-		callsiteRp = rp
-		break
-	}
-
-	if callsiteRp == 0 {
-		s.logger.Warn("no non-libc ")
-		return LibcPath, nil
-	}
-
-	lib, err := s.maps.AssignPC(callsiteRp, pid, dirty)
-	if err != nil {
-		return "", fmt.Errorf("failed to assign library to first non-libc rp: %w", err)
-	}
-
-	return lib, nil
-}
-
-func (s *StackParser) loadLibcRange(pid int32, dirty bool) (*libcRange, error) {
-	var rge *libcRange
-
-	rge, ok := s.libcRanges[pid]
-	if ok && !dirty {
-		return rge, nil
-	}
-
-	mmaps, err := s.maps.ReadAddrSpace(pid, dirty)
-	if err != nil {
-		return nil, fmt.Errorf("couldn't load address space: %w", err)
-	}
-
-	for _, m := range mmaps {
-		if m.PathName != LibcPath {
+		if lib == LibcPath {
 			continue
 		}
 
-		return &libcRange{m.AddrStart, m.AddrEnd}, nil
+		return lib, nil
 	}
 
-	return nil, ErrNoLibc
+	if lib != LibcPath {
+		return "A", fmt.Errorf("failed to assign syscall site to library: %w", ErrNoMappingExists)
+	}
+
+	return LibcPath, nil
 }
--- addrspace/stackparse_test.go
+++ addrspace/stackparse_test.go
@@ -14,33 +14,38 @@ import (
 func TestStackparseAssignPC(t *testing.T) {
 	cases := []struct {
 		name     string
-		trace    []uint64
+		trace    [64]uint64
 		expected string
 	}{
 		{
 			name:     "only libc, one frame",
-			trace:    []uint64{0x7ffff75b0000, 0, 0, 0}, // stack traces come in 0-padded lengths of powers of two
+			trace:    [64]uint64{0x7ffff75b0000}, // stack traces come in 0-padded lengths of powers of two
 			expected: "/usr/lib/x86_64-linux-gnu/libc.so.6",
 		},
 		{
 			name:     "only libc, two frames",
-			trace:    []uint64{0x7ffff75b0000, 0x7ffff75b0004, 0, 0},
+			trace:    [64]uint64{0x7ffff75b0000, 0x7ffff75b0004},
 			expected: "/usr/lib/x86_64-linux-gnu/libc.so.6",
 		},
 		{
 			name:     "libssl via libc",
-			trace:    []uint64{0x7ffff75b0000, 0x7ffff75b0004, 0x7ffff7e33000, 0},
+			trace:    [64]uint64{0x7ffff75b0000, 0x7ffff75b0004, 0x7ffff7e33000},
 			expected: "/usr/lib/x86_64-linux-gnu/libssl.so.3",
 		},
 		{
 			name:     "anonymous, one frame",
-			trace:    []uint64{0x7ffff7fbd000},
+			trace:    [64]uint64{0x7ffff7fbd000},
 			expected: "anonymous",
 		},
 		{
 			name:     "anonymous via libc",
-			trace:    []uint64{0x7ffff75b0000, 0x7ffff7fbd000},
+			trace:    [64]uint64{0x7ffff75b0000, 0x7ffff7fbd000},
 			expected: "anonymous",
+		},
+		{
+			name:     "nginx via libc",
+			trace:    [64]uint64{0x7ffff75b0000, 0x55555567c000},
+			expected: "/usr/sbin/nginx",
 		},
 	}
 
