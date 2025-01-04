db62d75d7a8de429aece24574a12563959422e60
Tom Cassar <cassar.thomas.e@gmail.com>
2025-01-04 11:54:45 +0000

    Create stats dir if it doesn't exist

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -18,14 +19,23 @@ func main() {
 	}
 
 	logger := prodLogger.Sugar()
+	defer logger.Sync()
 
 	maps := addrspace.NewProcMaps(logger)
 	stackparser := addrspace.NewStackParser(logger, &maps)
 
+	if len(os.Args) < 2 {
+		fmt.Println("Failed to run: please provide an executable followed by 0 or more arguments")
+		os.Exit(1)
+	}
+
 	executable := os.Args[1]
 	args := os.Args[2:]
 
-	reporter := syso.NewMTReporter(logger)
+	reporter, err := syso.NewMTReporter(logger)
+	if err != nil {
+		logger.Fatalw("failed to create reporter", "err", err)
+	}
 
 	// todo: timeout and jobs as cli arguments
 	tracer, err := syso.NewTracer(logger, stackparser, reporter, 6000*time.Second, 1)
--- syso/reporter.go
+++ syso/reporter.go
@@ -2,12 +2,16 @@ package syso
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
+	"path"
 	"sync"
 
 	"go.uber.org/zap"
 )
+
+var ErrCannotWriteStats = errors.New("cannot write stats to disk")
 
 type Reporter interface {
 	Report(stat *Stat)
@@ -25,11 +29,31 @@ type mtReporter struct {
 // NewMTReporter is a thread safe reporter that ignores timestamps.
 //
 // The reporter associates a library with a syscall number and a count.
-func NewMTReporter(logger *zap.SugaredLogger) Reporter {
+func NewMTReporter(logger *zap.SugaredLogger) (Reporter, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get current working directory: %w", err)
+	}
+
+	info, err := os.Stat(path.Join(wd, "stats"))
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return nil, fmt.Errorf("failed to check if ./stats exists: %w", err)
+	}
+
+	if errors.Is(err, os.ErrNotExist) {
+		if err := os.Mkdir("./stats", 0o777); err != nil {
+			return nil, fmt.Errorf("%w: couldn't make stats directory: %w", ErrCannotWriteStats, err)
+		}
+	}
+
+	if !info.IsDir() {
+		return nil, fmt.Errorf("%w: non-directory file called stats present", ErrCannotWriteStats)
+	}
+
 	return &mtReporter{
 		logger: logger,
 		stats:  make(map[string]map[uint64]int),
-	}
+	}, nil
 }
 
 func (u *mtReporter) Report(stat *Stat) {
@@ -51,6 +75,7 @@ func (u *mtReporter) Report(stat *Stat) {
 }
 
 func (u *mtReporter) WriteMissed(filepath string, missed *MissedStats) error {
+	u.logger.Infow("saving missed stats to disk", "path", filepath)
 	bts, err := json.Marshal(missed)
 	if err != nil {
 		return fmt.Errorf("failed to marshall stats: %w", err)
@@ -64,10 +89,10 @@ func (u *mtReporter) WriteMissed(filepath string, missed *MissedStats) error {
 }
 
 func (u *mtReporter) WriteFile(filepath string) error {
+	u.logger.Infow("saving count stats", "path", filepath)
+
 	u.mu.Lock()
 	defer u.mu.Unlock()
-
-	u.logger.Infow("saving count stats")
 
 	bts, err := json.Marshal(u.stats)
 	if err != nil {
